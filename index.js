a468081e62b44f1dce297e166b89a7430c2e6e78
RosaliaNjoki <njosali0@gmail.com>
2023-12-28 18:07:27 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const element = document.querySelector("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = "YOUR-NAME is the champion";
+// newHeader.idName = "victory";
+newHeader.setAttribute("id", "victory");
+
+
+
+
+
+
+
+
